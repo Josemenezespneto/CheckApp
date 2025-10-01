583ecd9dce62f575035f7e23dc49a5ad9044c3aa
Josemenezespneto <menezespn@gmail.com>
2025-10-01 15:57:34 -0300

    header so aparece se usuario logado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 // src/App.jsx
+import { useSelector } from 'react-redux';
 import './App.css'
 import Header from './components/header.jsx';
 import AppRoutes from "./router.jsx";
 
 
-
 function App() {
+  const loggedIn = useSelector((s) => s.auth.loggedIn);
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      {loggedIn ? (<Header />) : (<></>)}
+
       <main className="flex-1">
         <AppRoutes />
       </main>
